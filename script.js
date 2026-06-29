1711f73f52caa53110e221baabdcf6358fd148cc
Raj01-create <rajendrakeshari.pal014183@rec.ac.in>
2026-06-29 23:25:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+let cartCount = localStorage.getItem("cartCount") || 0;
+
+      const cartLink = document.querySelector("nav a:nth-child(4)");
+      cartLink.textContent = `Cart 🛒 (${cartCount})`;
+
+      const buttons = document.querySelectorAll("section:last-of-type button");
+
+      buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+          cartCount++;
+          localStorage.setItem("cartCount", cartCount);
+
+          cartLink.textContent = `Cart 🛒 (${cartCount})`;
+
+          alert("✅ Product added to cart!");
+        });
+      });
